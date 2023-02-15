4514f25d49ad780b5c79b86fd075b90f0865b334
lc-masa <myamanakalife6911@gmail.com>
2023-02-15 17:02:00 +0900

    updated theme mixin and moved theme class to html instead of body

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const currentTheme = localStorage.getItem("theme");
 
 // Apply the saved theme on page load
 if(currentTheme === "dark-theme"){
-  document.body.classList.add("dark-theme")
+  document.documentElement.classList.add("dark-theme")
 }else{
-  document.body.classList.add("light-theme")
+  document.documentElement.classList.add("light-theme")
 }
 
 toggleBtn.addEventListener("click", function() {
-  // Toggle the "dark-theme" class on the body
-  document.body.classList.toggle("dark-theme");
-  document.body.classList.toggle("light-theme");
+  // Toggle the "dark-theme" class on the documentElement (ie. html)
+  document.documentElement.classList.toggle("dark-theme");
+  document.documentElement.classList.toggle("light-theme");
   // Get the current theme
-  const currentTheme = document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme";
+  const currentTheme = document.documentElement.classList.contains("dark-theme") ? "dark-theme" : "light-theme";
   // Save the current theme in localStorage
   localStorage.setItem("theme", currentTheme);
 });
